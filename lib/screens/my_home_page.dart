b91258ba26b5394f5b96212a55542b4a8680835e
Theres Francis <theresmariafrancis@gmail.com>
2024-04-16 13:47:52 -0600

    update

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,18 +11,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  final List<String> tasks = <String>[];
+
+  final List<bool> checkboxes = List.generate(8, (index) => false);
+
+  bool isChecked = false;
+
+  /*
+  The TextEditingController class allows us to 
+  grab the input from the TextField() widget
+  This will be used later on to store the value
+  in the database.
+  */
+
   TextEditingController nameController = TextEditingController();
+
+  void addItemToList() async {
+    final String taskName = nameController.text;
+
+    //Add to the Firestore collection
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': null,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+
+    setState(() {
+      tasks.insert(0, taskName);
+      checkboxes.insert(0, false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 80.0,
-        backgroundColor: Color.fromARGB(255, 187, 103, 244),
+        backgroundColor: Color.fromARGB(255, 198, 120, 232),
+
+        /*
+            Rows() and Columns() both have the mainAxisAlignment 
+            property we can utilize to space out their child 
+            widgets to our desired format.
+           */
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             SizedBox(
+              /*
+            SizedBox allows us to control the vertical 
+            and horizontal dimensions by manipulating the 
+            height or width property, or both.
+            */
               height: 80,
               child: Image.asset('assets/rdplogo.png'),
             ),
@@ -29,7 +72,6 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(
                 fontFamily: 'Caveat',
                 fontSize: 32,
-                color: Colors.white,
               ),
             ),
           ],
@@ -38,14 +80,23 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         color: Colors.grey[200],
         child: Column(
-          children: <Widget>[
+          children: [
             Expanded(
               child: Container(
                 height: 300,
                 color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.all(7.0),
-                  child: TableCalendar(
+                  padding: const EdgeInsets.all(12.0),
+                  child:
+                      /*
+          The TableCalendar() widget below is installed via 
+          "flutter pub get table_calendar" or by adding the package 
+          to the pubspec.yaml file.  We then import it and implement using
+          configuration properties.  You can set a range and a focus day. 
+          The particulars of implementation for any package can be gleaned 
+          from pub.dev: https://pub.dev/packages/table_calendar.
+          */
+                      TableCalendar(
                     calendarFormat: CalendarFormat.month,
                     headerVisible: false,
                     focusedDay: DateTime.now(),
@@ -55,34 +106,100 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 100,
-                    width: 350,
-                    child: TextField(
-                      maxLength: 20,
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: 'Add To-Do List Item',
+
+            // comment what happens here...
+            ListView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(4),
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: checkboxes[index]
+                            ? Colors.green.withOpacity(0.7)
+                            : Colors.blue.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            !checkboxes[index]
+                                ? Icons.manage_history
+                                : Icons.playlist_add_check_circle,
+                            size: 32,
+                          ),
+                          SizedBox(width: 18),
+                          Text(
+                            '${tasks[index]}',
+                            style: checkboxes[index]
+                                ? TextStyle(
+                                    decoration: TextDecoration.lineThrough,
+                                    fontSize: 20,
+                                    color: Colors.black.withOpacity(0.5))
+                                : TextStyle(fontSize: 20),
+                          ),
+                          Checkbox(
+                            value: checkboxes[index],
+                            onChanged: (newValue) {
+                              setState(() {
+                                checkboxes[index] = newValue!;
+                              });
+                            },
+                          ),
+                        ],
                       ),
                     ),
+                  );
+                }),
+            Padding(
+              padding: const EdgeInsets.only(top: 12, left: 25, right: 25),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: TextField(
+                        controller: nameController,
+                        style: TextStyle(fontSize: 18),
+                        maxLength: 20,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          labelText: 'Add To-Do List Item',
+                          labelStyle: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                          ),
+                          hintText: 'Enter your task here',
+                          hintStyle:
+                              TextStyle(fontSize: 16, color: Colors.grey),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: null,
                   ),
                 ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
-                onPressed: null,
-                child: Text('Submit'),
+                onPressed: () {
+                  addItemToList();
+                },
+                child: Text('Add To-Do Item'),
               ),
-            ),
+            )
           ],
         ),
       ),
